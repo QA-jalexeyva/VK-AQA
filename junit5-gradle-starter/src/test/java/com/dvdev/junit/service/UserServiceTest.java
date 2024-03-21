0047fbf05f929206621bfb0b813fc32f0bebfe31
Alexey Archakov <jaalexeyva@mail.ru>
2024-03-21 16:55:31 +0300

    o da, ya eto sdelal

---
+++ junit5-gradle-starter/src/test/java/com/dvdev/junit/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.dvdev.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        assertTrue( true);
+    }
+}
