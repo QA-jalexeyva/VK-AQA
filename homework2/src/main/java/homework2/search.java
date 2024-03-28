200d059db7713516b16bd58800eb3d9997e9802e
Alexey Archakov <jaalexeyva@mail.ru>
2024-03-28 15:48:58 +0300

    da_ya_v_7_klasse

---
+++ homework2/src/main/java/homework2/search.java
@@ -0,0 +1,20 @@
+package homework2;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+
+
+// тест поисковых запросов на главной странице авторизации
+
+public class search {
+
+    @Test
+    public void search () {
+        open("https://ok.ru/");     // открыть сайт
+        $(By.name("st.query"))                      // найти поле поиска
+                .setValue("Мистер Дудец")           // ввести значение
+                .pressEnter();                      // нажать кнопку Enter
+        sleep(4_000);                    // эххх, хорошая мелодия.....
+    }                                              // я заплакал
+}
